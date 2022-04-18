f8376671bc41ad157f5a71d865963aad2ad6d981
Ramzi H <18570300+rhijjawi@users.noreply.github.com>
2022-04-18 21:43:12 +0300

    Update spotify.js

--- src/ts/spotify.js
+++ src/ts/spotify.js
@@ -1,9 +1,22 @@
-let session;
+let session_token;
 
 const _ = setInterval(async function() {
-if (session == undefined) {
+if (session_token == undefined) {
   console.log("Spotify not initialised")
 }
 else {let spotify = true}
 }, 1000)
                       
+const i = setInterval(async function() {
+if (spotify == true) {
+  let config = {
+    headers: {
+        Authorization: `Bearer ${session_token}`,
+        'Content-Type' : 'application/json'
+    }
+  }
+  let data = {"context_uri": "spotify:album:5ht7ItJgpBH7W6vJ5BqpPr","offset": {"position": 5},"position_ms": 0}
+  axios.put('https://npbe.ramzihijjawi.me/song', data, config)
+}
+else {}
+}, 1000)

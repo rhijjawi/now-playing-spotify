bd576089bb9a7d1942504f8341ea8f90e54b4862
Ramzi H <18570300+rhijjawi@users.noreply.github.com>
2022-04-18 21:27:48 +0300

    Update spotify.js

--- src/ts/spotify.js
+++ src/ts/spotify.js
@@ -1,24 +1,9 @@
 let session;
-function prep() {
-  window.open('https://npbe.ramzihijjawi.me/login', '_blank');
-  let text = document.getElementById('session')
-  text.style.hidden = false
-  //let ico = document.getElementsByTagName('img')[1]
 
-  text.addEventListener("keyup", function(event) {
-  // Number 13 is the "Enter" key on the keyboard
-  if (event.keyCode === 13) {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    let session = text.val
-    text.style.hidden = true
-    
-  }
-});
-}
 const _ = setInterval(async function() {
-if (session !== undefined) {
+if (session == undefined) {
   console.log("Spotify not initialised")
 }
+else {let spotify = true}
 }, 1000)
                       

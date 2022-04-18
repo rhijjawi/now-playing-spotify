84719b8d8b36a63c3a61d98e1e837550952a100a
Ramzi H <18570300+rhijjawi@users.noreply.github.com>
2022-04-18 22:04:28 +0300

    Update spotify.js

--- src/ts/spotify.js
+++ src/ts/spotify.js
@@ -7,9 +7,6 @@ if (session_token == '') {
   console.log("Spotify not initialised")
 }
 else {let spotify = true}
-}, 1000)
-                      
-const i = setInterval(async function() {
 if (spotify == true) {
   let text = document.getElementById('session')
   let session_token = text.value
@@ -20,7 +17,7 @@ if (spotify == true) {
     }
   }
   let data = {"context_uri": "spotify:album:5ht7ItJgpBH7W6vJ5BqpPr","offset": {"position": 5},"position_ms": 0}
-  axios.put('https://npbe.ramzihijjawi.me/song', data, config)
+  await axios.put('https://npbe.ramzihijjawi.me/song', data, config)
 }
 else {}
 }, 1000)

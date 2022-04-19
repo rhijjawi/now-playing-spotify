964f9d33b5833d2f8f2018b17f9d618c085baf5c
Ramzi H <18570300+rhijjawi@users.noreply.github.com>
2022-04-19 13:18:10 +0300

    Update main.js

--- src/ts/main.js
+++ src/ts/main.js
@@ -41,56 +41,57 @@ const interval = setInterval(async function() {
   getDataJSON = getData.data
   
   if(getDataJSON.hasOwnProperty('status')){
-  if (getDataJSON.status == 'not_playing') {} else {}}
+    if (getDataJSON.status == 'not_playing') {changeIfChanged($('#title'), `<a>Not Playing</a>`)} 
+    else {}
+    }
   else {
-  let volume = `@ ${getDataJSON.device.volume_percent}% volume`
-  let playingStatement = `Playing on <span class="bold">${getDataJSON['device']['name']}</span> ${volume}`
-  changeImageIfChanged($('#album-art'), getDataJSON.item.album.images[0].url)
-  changeIfChanged($('#title'), `<a href="${getDataJSON.item.external_urls.spotify}">${getDataJSON.item.name}</a>`)
-  changeIfChanged(
-    $('#album'),
-    `<span class="muted">on</span> <a href="${getDataJSON.item.album.external_urls.spotify}">${getDataJSON.item.album.name}</a>`,
-  )
-  changeIfChanged(
-    $('#artist'),
-    `<span class="muted">by</span> <a href="${getDataJSON.item.album.artists[0].external_urls.spotify}">${getDataJSON.item.album.artists[0].name}</a>`,
-  )
-  if (getDataJSON.is_playing) {
-    stillPaused = false;
-    
-    changeIfChanged($('#status'), playingStatement)
-    addClass($('#album-art'), 'spin')
-    removeClass($('#album-art'), 'pause-spin')
-  }
-  if (!getDataJSON.is_playing) {
-    changeIfChanged($('#status'), 'Paused')
-    addClass($('#album-art'), 'pause-spin')
-    pause();
-  }
-  bar = document.getElementById('progressBar')
-  bar.style.width = `${(getDataJSON.progress_ms/getDataJSON.item.duration_ms)*100}%`
-  
-  
-  
-  let text = document.getElementById('session')
-  let session_token = text.value
-  let currentSong = '';
-  if (session_token == '') {}
-  else {spotify = true}
-  if (spotify == true) {
+    let volume = `@ ${getDataJSON.device.volume_percent}% volume`
+    let playingStatement = `Playing on <span class="bold">${getDataJSON['device']['name']}</span> ${volume}`
+    changeImageIfChanged($('#album-art'), getDataJSON.item.album.images[0].url)
+    changeIfChanged($('#title'), `<a href="${getDataJSON.item.external_urls.spotify}">${getDataJSON.item.name}</a>`)
+    changeIfChanged(
+      $('#album'),
+      `<span class="muted">on</span> <a href="${getDataJSON.item.album.external_urls.spotify}">${getDataJSON.item.album.name}</a>`,
+    )
+    changeIfChanged(
+      $('#artist'),
+      `<span class="muted">by</span> <a href="${getDataJSON.item.album.artists[0].external_urls.spotify}">${getDataJSON.item.album.artists[0].name}</a>`,
+    )
+    if (getDataJSON.is_playing) {
+      stillPaused = false;
+      changeIfChanged($('#status'), playingStatement)
+      addClass($('#album-art'), 'spin')
+      removeClass($('#album-art'), 'pause-spin')
+    }
+    if (!getDataJSON.is_playing) {
+      changeIfChanged($('#status'), 'Paused')
+      addClass($('#album-art'), 'pause-spin')
+      pause();
+    }
+    bar = document.getElementById('progressBar')
+    bar.style.width = `${(getDataJSON.progress_ms/getDataJSON.item.duration_ms)*100}%`
 
-  let text = document.getElementById('session')
-  let session_token = text.value
-  
-  if (getDataJSON.item.uri !== currentSong) {
-      let data = {"session": `${session_token}`, "uris": [getDataJSON.item.uri], "offset": {"position": 0},"position_ms": getDataJSON.progress_ms}
-      await axios.put('https://npbe.ramzihijjawi.me/song', data, config)
-  }
-  else {}
-  currentSong = getDataJSON.item.uri
-  console.log(`currSong = ${currentSong}`);
-  console.log(`currItemuri = ${getDataJSON.item.uri}`);
-  console.log(currentSong == getDataJSON.item.uri)  
+
+
+    let text = document.getElementById('session')
+    let session_token = text.value
+    let currentSong = '';
+    if (session_token == '') {}
+    else {spotify = true}
+    if (spotify == true) {
+
+    let text = document.getElementById('session')
+    let session_token = text.value
+
+    if (currentSong !== getDataJSON.item.uri) {
+        let data = {"session": `${session_token}`, "uris": [getDataJSON.item.uri], "offset": {"position": 0},"position_ms": getDataJSON.progress_ms}
+        await axios.put('https://npbe.ramzihijjawi.me/song', data, config)
+    }
+    else {console.log('false false false')}
+    currentSong = getDataJSON.item.uri
+    console.log(`currSong = ${currentSong}`);
+    console.log(`currItemuri = ${getDataJSON.item.uri}`);
+    console.log(currentSong == getDataJSON.item.uri)  
 }
 
   

aa3ac1990a397209ada088c06c0f7c822b7f4e90
Ramzi H <18570300+rhijjawi@users.noreply.github.com>
2022-04-24 12:37:03 +0300

    addEvent('click')

--- src/ts/spotify.js
+++ src/ts/spotify.js
@@ -1,5 +1,1 @@
-async function makeroom() {
-    let r = await axios.post('https://npbe.ramzihijjawi.me/rooms', {withCredentials: true})
-    document.cookie = r.data.set
-    window.open(r.data.redir)
-}
+

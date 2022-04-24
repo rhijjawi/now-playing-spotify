a03ee33cd0a8d989ef70200241187177350154cd
Ramzi H <18570300+rhijjawi@users.noreply.github.com>
2022-04-24 12:45:58 +0300

    Patch

--- src/ts/spotify.js
+++ src/ts/spotify.js
@@ -1,1 +1,5 @@
-
+document.getElementById('makeroom').addEventListener('click', async function() {
+    let r = await axios.post('https://npbe.ramzihijjawi.me/rooms', {withCredentials: true})
+    document.cookie = r.data.set
+    window.open(r.data.redir)
+});

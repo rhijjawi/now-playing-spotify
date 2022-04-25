ef9110c448bdf4623adaac5f30a344142e93eccf
Ramzi H <18570300+rhijjawi@users.noreply.github.com>
2022-04-25 20:41:00 +0300

    Steven Hee

--- src/ts/general_functions.js
+++ src/ts/general_functions.js
@@ -27,7 +27,7 @@ document.getElementById('makeroom').addEventListener('click', async function mak
     window.open(r.data.redir)
 });
 
-document.addEventListener('load', async function getUser() {
+document.addEventListener('DOMContentLoaded', async function getUser() {
     let config = {headers: {'Content-Type' : 'application/json','Authorisation' : 'Bearer 0000000-00000000-0000000'}}
     if(document.cookie.split(document.cookie.split('spotify=')[1] !== undefined)) {
         let r = await axios.post('https://npbe.ramzihijjawi.me/me', {'cookie': `${document.cookie.split('spotify=')[1].split(';')[0]}`}, config)

2652afd2db4cb639b263c589fbe4344f1381915a
Ramzi H <18570300+rhijjawi@users.noreply.github.com>
2022-05-06 11:50:48 +0300

    SCROLLING SUCKS

--- src/ts/spotify.js
+++ src/ts/spotify.js
@@ -1,18 +1,3 @@
-document.addEventListener("DOMContentLoaded", function(event) { //Submit on Enter
-    let text = document.getElementById('session')
-    text.addEventListener("keyup", function(event) {
-    // Number 13 is the "Enter" key on the keyboard
-    if (event.keyCode === 13) {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    let session_token = text.value
-    document.cookie = `spotify=${session_token}`
-    console.log('Logged in as:')
-    console.log(session_token)
-    text.hidden = true
-}})
-});
-
 async function prep() {
     if(document.cookie.split('spotify=')[1] == undefined) {
         window.open('https://npbe.ramzihijjawi.me/login', '_self')

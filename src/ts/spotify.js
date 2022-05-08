992ef6769cd7a5fbc61785008355babb35a9d675
Ramzi H <18570300+rhijjawi@users.noreply.github.com>
2022-05-08 22:01:56 +0300

    push

--- src/ts/spotify.js
+++ src/ts/spotify.js
@@ -1,9 +1,7 @@
-async function prep() {
-    if(document.cookie.split('spotify=')[1] == undefined) {
-        window.open('https://npbe.ramzihijjawi.me/login', '_self')
-    }
-    else{}
-    }
-document.getElementById('prep_btn').addEventListener('click',prep());
-
-      
+//async function prep() {
+//    if(document.cookie.split('spotify=')[1] == undefined) {
+//        window.open('https://npbe.ramzihijjawi.me/login', '_self')
+//    }
+//    else{}
+//    }
+//document.getElementById('prep_btn').addEventListener('click',prep());

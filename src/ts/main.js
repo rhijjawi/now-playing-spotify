73127a15dcf7be2a4e6d5be3c645274e923989e7
Ramzi H <18570300+rhijjawi@users.noreply.github.com>
2022-05-12 12:30:39 +0300

    oop

--- src/ts/main.js
+++ src/ts/main.js
@@ -3,7 +3,7 @@ var socket = io('https://npbe.ramzihijjawi.me', {closeOnBeforeunload:false});
 let bar = $('#progressBar');
 let spotify = false;
 let config = {headers: {'Content-Type' : 'application/json','Authorisation' : 'Bearer 0000000-00000000-0000000'}}
-let stillPaused = playling = null;
+let stillPaused = playing = null;
 let currentSong = '';
 let playlists = await axios.get('https://npbe.ramzihijjawi.me/playlist');
 playlists = playlists.data

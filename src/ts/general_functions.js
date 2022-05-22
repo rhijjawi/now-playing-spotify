c899311f12a3e5a4907ac3e4502de0db39a96fdf
Ramzi H <18570300+rhijjawi@users.noreply.github.com>
2022-05-22 21:14:49 +0300

    del redundant code in gen_func

--- src/ts/general_functions.js
+++ src/ts/general_functions.js
@@ -1,27 +1,5 @@
 let $ = (selector) => document.querySelector(selector);
 var socket = io('https://npbe.ramzihijjawi.me');
-function changeIfChanged(el, content) {
-    if (el.innerHTML !== content) {
-      el.innerHTML = content
-    }
-  }
-  
-function changeImageIfChanged(el, url) {
-if (el.src !== url) {
-    el.src = url
-}
-}
-
-function addClass(el, className) {
-if (!el.classList.contains(className)) {
-    el.classList.add(className)
-}
-}
-
-function removeClass(el, className) {
-if (el.classList.contains(className)) {
-    el.classList.remove(className)
-}}
 
 function rmEventListener(func) {
     document.getElementById('prep_btn').removeEventListener('click', func)

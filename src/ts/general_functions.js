1045eacafa91ef20695702cf1bb3a7d3814622ad
Ramzi H <18570300+rhijjawi@users.noreply.github.com>
2022-06-05 19:15:13 +0300

    yuh

--- src/ts/general_functions.js
+++ src/ts/general_functions.js
@@ -1,3 +1,25 @@
 let $ = (selector) => document.querySelector(selector);
 var socket = io('https://npbe.ramzihijjawi.me');
 
+function changeIfChanged(el, content) {
+    if (el.innerHTML !== content) {
+      el.innerHTML = content
+    }
+  }
+  
+function changeImageIfChanged(el, url) {
+if (el.src !== url) {
+    el.src = url
+}
+}
+
+function addClass(el, className) {
+if (!el.classList.contains(className)) {
+    el.classList.add(className)
+}
+}
+
+function removeClass(el, className) {
+if (el.classList.contains(className)) {
+    el.classList.remove(className)
+}}
